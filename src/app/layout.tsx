e2271ac6f0ead225378cebd14955a96edb13be96
aram hernandez <crypticmorning@outlook.com>
2024-04-03 00:32:05 -0700

    Add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,38 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Script from "next/script";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Aram Portfolio',
-  description: 'Welcome to my profolio and brand!',
-}
+  title: "Aram Portfolio",
+  description: "Welcome to my profolio and brand!",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
+      <head>
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-FJ2E7XE4RF"
+        />
+        <Script>
+          {`window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-FJ2E7XE4RF');`
+          }
+        </Script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
